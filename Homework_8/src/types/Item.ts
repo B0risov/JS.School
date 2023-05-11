6a9331e286a05e9b46d9ea699aecb8249060fe36
Borisov <sendetskymatvei@yandex.ru>
2023-05-11 12:05:14 +0400

    delete node modules and package-lock.json

--- Homework_8/src/types/Item.ts
+++
@@ -1,7 +0,0 @@
-export interface Item {
-    id: string,
-    isChecked: boolean,
-    title: string,
-    tag: string,
-    date: string,
-}
--- Homework_8/src/types/State.ts
+++
@@ -1,6 +0,0 @@
-export interface WeatherState {
-    city: string,
-    temp_c: string,
-    weather_icon: string,
-    weather_text: string
-}
